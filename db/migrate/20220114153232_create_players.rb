2266acf2870069a371f8ace41435a2bc63f5cc81
Hristo Tenev <htenev@clustermarket.com>
2022-01-14 17:33:33 +0200

    create few test models

---
+++ db/migrate/20220114153232_create_players.rb
@@ -0,0 +1,14 @@
+class CreatePlayers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :players do |t|
+      t.belongs_to :team, null: false, foreign_key: true
+      t.string :first_name
+      t.string :last_name
+      t.integer :age
+      t.integer :number
+      t.string :possition
+
+      t.timestamps
+    end
+  end
+end
